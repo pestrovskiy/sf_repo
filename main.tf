2c6fdf05d4469d8abd44c0eaaa49386a10324377
pestrovskiy <a.pestrovskiy@gmail.com>
2022-05-05 19:42:49 +0300

    B 5.7

---
+++ main.tf
@@ -0,0 +1,81 @@
+
+terraform {
+  required_providers {
+    yandex = {
+      source = "terraform-registry.storage.yandexcloud.net/yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = "AQAAAABeYUXWAATuwYyk5P3_gUNokzkH2LK457A"
+  cloud_id  = "b1g290kvknp80brbg7jh"
+  folder_id = "b1grur211fafb9ufoivk"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "network"
+}
+
+resource "yandex_vpc_subnet" "subnet1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+module "ya_instance_1" {
+  source                = "./modules/instance"
+  instance_family_image = "lemp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet1.id
+
+}
+
+module "ya_instance_2" {
+  source                = "./modules/instance"
+  instance_family_image = "lamp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet1.id
+}
+
+
+resource "yandex_lb_target_group" "target-group" {
+  name      = "my-target-group"
+  region_id = "ru-central1"
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet1.id
+    address   = module.ya_instance_1.internal_ip_address_vm
+  }
+
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet1.id
+    address   = module.ya_instance_2.internal_ip_address_vm
+  }
+
+}
+
+resource "yandex_lb_network_load_balancer" "lb1" {
+  name = "my-network-load-balancer"
+  listener {
+    name = "my-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.target-group.id
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+  }
+}
---
+++ modules/instance/variables.tf
@@ -0,0 +1,12 @@
+variable "instance_family_image" {
+  description = "Instance image"
+  type        = string
+  default     = "lamp"
+}
+
+variable "vpc_subnet_id" {
+  description = "VPC subnet network id"
+  type        = string
+}
+
+
